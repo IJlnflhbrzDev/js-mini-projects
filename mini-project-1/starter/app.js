8b9d38b2142056529c0138813b6b4102d19b3aa7
IJlal Naufal Hibrizi <IJlnflhbrzDev@gmail.com>
2023-10-12 21:19:13 +0700

    change title in dom, Aritmatic prosedure

---
+++ mini-project-1/starter/app.js
@@ -0,0 +1,45 @@
+// Memangil id bernama headingTitle 
+const projectTitle = document.querySelector('#headingTitle');
+// Mengubah title 
+projectTitle.textContent =  'Title Baru Dari JS File';
+
+
+var thisIsFunction = 'This is Function';
+const PI = 3.14 ;
+
+var y = 10;
+var z = 5;
+
+var x = y + z;
+
+// function di bawah ini akan mereturn hasil dari penjumlahan a + b;
+function sumNum(a, b) {
+    return a + b ;
+}
+
+// var iniSebuahVar = 'var';
+/*
+ini adalah comment
+dengan baris
+lebih dari satu 
+*/
+IniSebuahVar = 'Variable Baru';
+
+console.log(iniSebuahVar);
+
+// Aritmatika 
+let additional = 1000 + 500 ; 
+
+let substruction = 1000 - 500 ;
+
+let multiplication = 100 * 5 ;
+
+let division = 100 / 2 ;
+
+// prosedure / order of aritmatic 
+let edgeCase = 1000 + (4 * 2); // yg di tanda kurung akan lebih dahulu di jalankan  
+
+// modulus atau reminder 
+let modulus = 230 % 100; 
+let even = 25 % 2; 
+console.log(edge)
